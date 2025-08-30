5eebfc43147054acf7fc813dec21fe45093669bc
clorensia <106355113+clorensia@users.noreply.github.com>
2025-08-30 12:09:40 +0700

    tamba if else logic

--- script.js
+++ script.js
@@ -8,4 +8,24 @@ calculateBtn.addEventListener('click', () => {
     const height = parseFloat(heightInput.value);
     const bmi = weight / Math.pow(height / 100, 2);
     const roundedBmi = bmi.toFixed(1); 
+
+    let category, color, categoryId;
+    if (bmi < 18.5) {
+        category = 'Kurus';
+        color = '#c2185b';
+        categoryId = 'category-underweight';
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+        category = 'Normal';
+        color = '#2e7d32';
+        categoryId = 'category-normal';
+    } else if (bmi >= 25 && bmi <= 29.9) {
+        category = 'Gemuk';
+        color = '#f9a825';
+        categoryId = 'category-overweight';
+    } else {
+        category = 'Obesitas';
+        color = '#8e24aa';
+        categoryId = 'category-obesity';
+    }
+
 })
